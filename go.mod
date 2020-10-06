5aeb0ceacf8f4cd79071582d2f506ff4e3eded47
wei <wei840222@gmail.com>
2020-10-06 08:09:06 +0800

    add server interceptor

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/wei840222/go-grpc-colorlog
+
+go 1.15
+
+require (
+	github.com/mattn/go-isatty v0.0.12
+	google.golang.org/grpc v1.32.0
+)
---
+++ interceptors.go
@@ -0,0 +1,102 @@
+package colorlog
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/mattn/go-isatty"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+const (
+	green   = "\033[97;42m"
+	white   = "\033[90;47m"
+	yellow  = "\033[90;43m"
+	red     = "\033[97;41m"
+	blue    = "\033[97;44m"
+	magenta = "\033[97;45m"
+	cyan    = "\033[97;46m"
+	reset   = "\033[0m"
+)
+
+type logPayload struct {
+	startTime     time.Time
+	fullMethod    string
+	statusCode    codes.Code
+	err           error
+	isOutputColor bool
+}
+
+func (l *logPayload) detectStatusCodeFromError(err error) {
+	l.err = err
+	l.statusCode = status.Code(l.err)
+}
+
+func (l logPayload) statusCodeColor() string {
+	switch {
+	case l.statusCode >= codes.OK && l.statusCode < codes.Canceled:
+		return green
+	case l.statusCode >= codes.Canceled && l.statusCode < codes.Unknown:
+		return white
+	case l.statusCode >= codes.Unknown && l.statusCode < codes.Unimplemented || l.statusCode == codes.Unauthenticated:
+		return yellow
+	default:
+		return red
+	}
+}
+
+func (l logPayload) print() {
+	var statusColor, methodColor, resetColor string
+	if l.isOutputColor {
+		statusColor = l.statusCodeColor()
+		methodColor = blue
+		resetColor = reset
+	}
+	var errMsg string
+	if l.err != nil {
+		errMsg = l.err.Error()
+	}
+	fmt.Printf("[gRPC-Go] %v |%s %19s %s| %13v | %s %-7s %s\n%s",
+		l.startTime.Format("2006/01/02 - 15:04:05"),
+		statusColor, l.statusCode.String(), resetColor,
+		time.Since(l.startTime),
+		methodColor, l.fullMethod, resetColor,
+		errMsg,
+	)
+}
+
+// UnaryServerInterceptor returns a new unary server interceptors.
+func UnaryServerInterceptor() grpc.UnaryServerInterceptor {
+	isTerm := isatty.IsTerminal(os.Stdout.Fd()) || isatty.IsCygwinTerminal(os.Stdout.Fd())
+	return func(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (interface{}, error) {
+		l := logPayload{
+			startTime:     time.Now(),
+			fullMethod:    info.FullMethod,
+			isOutputColor: isTerm,
+		}
+		res, err := handler(ctx, req)
+		l.detectStatusCodeFromError(err)
+		l.print()
+		return res, err
+	}
+}
+
+// StreamServerInterceptor returns a new streaming server interceptor.
+func StreamServerInterceptor() grpc.StreamServerInterceptor {
+	isTerm := isatty.IsTerminal(os.Stdout.Fd()) || isatty.IsCygwinTerminal(os.Stdout.Fd())
+	return func(srv interface{}, ss grpc.ServerStream, info *grpc.StreamServerInfo, handler grpc.StreamHandler) error {
+		l := logPayload{
+			startTime:     time.Now(),
+			fullMethod:    info.FullMethod,
+			isOutputColor: isTerm,
+		}
+		err := handler(srv, ss)
+		l.detectStatusCodeFromError(err)
+		l.print()
+		return err
+	}
+}
